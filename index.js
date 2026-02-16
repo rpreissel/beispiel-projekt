be63ed61b95e090d9ef560de64063837109dc8dc
Rene Preissel <rene.preissel@etosquare.de>
2026-02-16 21:47:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const greeting = (name) => {
+  return `Hallo, ${name}! Willkommen bei Node.js.`;
+};
+
+console.log(greeting('Welt'));
+console.log('Das Beispielprojekt wurde erfolgreich gestartet.');
